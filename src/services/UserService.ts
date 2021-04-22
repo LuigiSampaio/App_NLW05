30fc1ea3639b4d16cef53adcc87db9a9f1f957cf
LuigiSampaio <cursosinformatica9102@gmail.com>
2021-04-22 08:53:19 -0300

    feat(migrations) creating migration sqlite day 03

---
+++ src/services/UserService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+import { User }  from "../entities/User";
+
+class UsersService {
+
+    private usersRepository: Repository<User>;
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+    
+
+    async create(email: string) {
+        // Verificar se usuário existe
+
+        const userExists = await this.usersRepository.findOne({
+            email
+        });
+
+        // Se Existir, retornar user
+        if(userExists) {
+            return userExists;
+        }
+
+        const user = this.usersRepository.create({
+            email
+        });
+
+        await this.usersRepository.save(user);
+        
+        // Se não existir, salvar no DB
+        return user;
+    }
+}
+
+export { UsersService }
